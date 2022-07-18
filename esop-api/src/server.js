ea65408f0b847d9ae63cd85a31b23c31adcc6920
paang48 <zigasemakin@gmail.com>
2022-07-18 11:12:46 +0700

    membuat server gta

---
+++ esop-api/src/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+  const server = Hapi.server({
+    host: process.env.HOST,
+    port: process.env.PORT,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      name: 'Farhan maulana A',
+    }),
+  });
+
+  await server.start();
+  console.log(`Server running at ${server.info.uri}`);
+
+};
+
+init()
